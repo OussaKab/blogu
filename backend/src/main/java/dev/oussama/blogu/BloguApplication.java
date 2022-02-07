bc0e283303ac5b9e03c626330c370ee033e2fdb0
OussaKab <oussamakably@gmail.com>
2022-02-06 22:46:04 -0500

    first commit

---
+++ backend/src/main/java/dev/oussama/blogu/BloguApplication.java
@@ -0,0 +1,13 @@
+package dev.oussama.blogu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloguApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/dev/oussama/blogu/BloguApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oussama.blogu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
